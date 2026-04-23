abe296e5300753cbbbc990d7d476fa5de5e81751
John T Carter <johnhexcarter@protonmail.com>
2026-04-23 14:48:55 -0400

    add dummy page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,6 +3,12 @@
 class SiteController < ApplicationController
   allow_unauthenticated_access
 
+  layout :check_for_dummy_page
+
   def index
+  end
+
+  def check_for_dummy_page
+    @dummy ? 'dummy' : 'application'
   end
 end
