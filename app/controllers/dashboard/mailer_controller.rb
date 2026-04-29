27eb97c39f19c28650f6eddea5aadc161ef01b1e
John T Carter <johnhexcarter@protonmail.com>
2026-04-29 10:48:55 -0400

    progres

--- app/controllers/dashboard/mailer_controller.rb
+++ app/controllers/dashboard/mailer_controller.rb
@@ -5,5 +5,18 @@ module Dashboard
     def index
       @confirmed = MailingListEntity.confirmed
     end
+
+    def new
+    end
+
+    def create
+      mailer = Mailer.new params.expect(mailer: [:subject, :body])
+
+      if mailer.valid?
+        redirect_to dashboard_mailer_path, notice: 'You successfully created a new mailer.'
+      else
+        redirect_to dashboard_new_mailer_path, alert: 'Something went wrong. Please try again.'
+      end
+    end
   end
 end
